a0adb9f3f0af1cf6dbd9c86dbb127094f1882873
undol26TW <undol26@twinny.co.kr>
2021-09-15 21:17:57 +0900

    Update shell

--- install_zshtmuxvim.sh
+++ install_zshtmuxvim.sh
@@ -68,39 +68,9 @@ cat $CUR_PATH/plugin_list >> ~/.vimrc
 
 vim +slient +VimEnter +PlugInstall +qall
 
-rm -rf ~/SimpleVimTutorial
-sudo chown -R twinny ~/.vim
+rm -rf SimpleVimTutorial
+sudo chown -R undol26 ~/.vim
 echo "************************************"
 echo ""
 
 
-# clang-format
-echo "clang-format install"
-sudo tee -a ~/.clang-format << EOF
- BasedOnStyle : Google
- AccessModifierOffset: -4
- ColumnLimit: 100
- IndentWidth:     4
- BreakBeforeBraces : Custom
- BraceWrapping:
-   AfterClass:      true
-   AfterControlStatement: false
-   AfterEnum:       true
-   AfterFunction:   true
-   AfterNamespace:  true
-   AfterObjCDeclaration: false
-   AfterStruct:     true
-   BeforeCatch:     true
-   BeforeElse:      true
-   IndentBraces:    false
- AllowShortFunctionsOnASingleLine: None
- AlignAfterOpenBracket: true
-EOF
-echo "************************************"
-echo ""
-
-# rqt_image_view install
-echo "rqt_image_view install"
-sudo apt-get install ros-melodic-rqt-image-view
-echo "************************************"
-echo ""
